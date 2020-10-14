8149eab6f15f0b405af2c9659efbaaa735b51714
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2020-10-13 18:58:56 -0600

    Update Dashboard.ps1

--- influxdb/Dashboard.ps1
+++ influxdb/Dashboard.ps1
@@ -1,57 +1,12 @@
-Import-Module C:\ProgramData\PowerShellUniversal\Dashboard\Components\UniversalDashboard.Charts\1.0.0\UniversalDashboard.Charts.psd1
 Import-Module "$PSScriptRoot\influxdb.psm1"
 
-New-UDDashboard -Title "Server Performance Dashboard" -Content {
+New-UDDashboard -Title 'Performance Data' -Content {
 
-New-UDRow -Columns {
-    New-UDColumn -SmallSize 12 -Content {
-        New-UDHeading -Text "$ENV:ComputerName" -Size 4
-    }
-}
+    New-UDGridLayout -Content {
+        New-UDCard -Id 'networkData' -Title 'Network Data' -Content {
+            New-UDElement -Tag 'div' -Attributes @{ style = @{ width = "100%"; height = "100%" }}  -AutoRefresh -RefreshInterval 5 -Endpoint {
 
-New-UDRow -Columns {
-    
-}
-
-New-UDRow -Columns {
-    New-UDColumn -SmallSize 12 -Content {
-        New-UDCard -Title "Performance Metrics" -Content {
-            New-UDElement -Tag 'div' -Attributes @{ style = @{ "height" = '400px'}} -AutoRefresh -RefreshInterval 5 -Endpoint {
-
-                $Data = Get-InfluxDb -Query 'SELECT * FROM counter WHERE time > now() - 5m GROUP BY counter'
-
-                $ChartData = @()
-
-                foreach($series in $data) {
-
-                    $SeriesData = @{
-                        id =  $series.counter
-                        data = @()
-                    }
-
-                    foreach($field in $Series.Fields) {
-                        $SeriesData.data += @{
-                            x = $field.time
-                            y = $field.value
-                        }                        
-                    }
-
-                    $ChartData += $SeriesData
-                }
-
-                $BottomAxis = New-UDNivoChartAxisOptions -TickRotation 90
-                New-UDNivoChart -Data $ChartData -Id "performanceStats" -Line -Responsive -MarginBottom 50 -MarginTop 50 -MarginRight 110 -MarginLeft 60 -YScaleMax 100 -YScaleMin 0 -EnableArea -AxisBottom $BottomAxis -Colors 'paired'
-            }
-        }
-    }
-}
-
-New-UDRow -Columns {
-    New-UDColumn -SmallSize 12 -Content {
-        New-UDCard -Title "Network Traffic" -Content {
-            New-UDElement -Tag 'div' -Attributes @{ style = @{ "height" = '400px'}} -AutoRefresh -RefreshInterval 5 -Endpoint {
-
-                $Data = Get-InfluxDb -Query 'SELECT * FROM network WHERE time > now() - 5m GROUP BY counter'
+                $Data = Get-InfluxDb -Query 'SELECT * FROM network WHERE time > now() - 60m GROUP BY counter'
 
                 $ChartData = for($i = 0; $i -lt $data[0].Fields.length; $i++) {
                     $Point = @{}
@@ -60,9 +15,54 @@ New-UDRow -Columns {
                     }
                     $Point
                 }
-                New-UDNivoChart -Stream -Data $ChartData -Id "networkData" -Responsive -MarginBottom 50 -MarginTop 50 -MarginRight 110 -MarginLeft 60 -Keys $Cache:NetworkStats -OffsetType expand -Curve linear  -Colors 'paired'
+
+                $NetworkStats = @(
+                    '\network adapter(*)\bytes received/sec'
+                    '\network adapter(*)\bytes sent/sec'
+                )
+
+                New-UDNivoChart -Height '250' -Stream -Data $ChartData -Id "networkData" -Responsive -MarginBottom 50 -MarginTop 50 -MarginRight 110 -MarginLeft 60 -Keys $NetworkStats -OffsetType expand -Curve linear
             }
         }
-    }
-}
+
+        New-UDCard -Id 'performanceData' -Title 'Performance Data' -Content {
+            New-UDElement -Tag 'div' -Attributes @{ style = @{ width = "100%"; height = "100%" }} -AutoRefresh -RefreshInterval 5 -Endpoint {
+
+                $Data = Get-InfluxDb -Query 'SELECT * FROM counter WHERE time > now() - 60m GROUP BY counter'
+
+                $ChartData = @()
+  
+      
+                foreach($series in $data) {
+
+                    $SeriesData = @{
+                        id =  $series.counter
+                        data = @()
+                    }
+                    
+                    $i = 0
+                    foreach($field in $Series.Fields) {
+                        $SeriesData.data += @{
+                            x = $i
+                            y = $field.value
+                        }               
+                        $i++         
+                    }
+
+                    $ChartData += $SeriesData
+                }
+
+                New-UDNivoChart -Height 250 -Data $ChartData -Id "performanceStats" -Line -Responsive -MarginBottom 50 -MarginTop 50 -MarginRight 110 -MarginLeft 60 -YScaleMax 100 -YScaleMin 0 -EnableArea
+            }
+        }
+
+        New-UDCard -Id 'diskData' -Title 'Disk Data' -Content {
+            New-UDElement -Tag 'div' -AutoRefresh -RefreshInterval 5 -Endpoint {
+
+                $Data = Get-InfluxDb -Query 'SELECT * FROM disk WHERE time > now() - 60m'
+
+                New-UDTable -Data $Data.Fields
+            }
+        }
+    } -Design
 }
