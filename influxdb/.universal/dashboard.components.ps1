20cce6b811bc0e91e0724c584ee34ec30d7e353d
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2020-10-13 19:00:25 -0600

    Update dashboard.components.ps1

--- influxdb/.universal/dashboard.components.ps1
+++ influxdb/.universal/dashboard.components.ps1
@@ -1,2 +1,3 @@
-New-PSUDashboardComponent -Name "UniversalDashboard.Charts" -Path "C:\ProgramData\PowerShellUniversal\Dashboard\Components\UniversalDashboard.Charts\1.0.0" -Version "1.0.0" 
+New-PSUDashboardComponent -Name "UniversalDashboard.Charts" -Path "C:\ProgramData\PowerShellUniversal\Dashboard\Components\UniversalDashboard.Charts\1.2.0" -Version "1.2.0" 
+New-PSUDashboardComponent -Name "UniversalDashboard.CodeEditor" -Path "C:\ProgramData\PowerShellUniversal\Dashboard\Components\UniversalDashboard.CodeEditor\1.0.4" -Version "1.0.4" 
 New-PSUDashboardComponent -Name "UniversalDashboard.Map" -Path "C:\ProgramData\PowerShellUniversal\Dashboard\Components\UniversalDashboard.Map\1.0" -Version "1.0"
